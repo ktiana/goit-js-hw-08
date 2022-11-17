af86fd23759d490d00559871217c147cf9e2005d
Tetiana Kornilova <ktianaa@gmail.com>
2022-11-17 12:54:39 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,14 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const VIDEO_KEY = 'videoplayer-current-time';
+const STORAGE_KEY = 'videoplayer-current-time';
 
 var throttle = require('lodash.throttle');
 
 function onPlay(data) {
-  localStorage.setItem(VIDEO_KEY, data.seconds);
+  localStorage.setItem(STORAGE_KEY, data.seconds);
 }
 
 player.on('timeupdate', throttle(onPlay, 1000));
-player.setCurrentTime(localStorage.getItem(VIDEO_KEY));
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
