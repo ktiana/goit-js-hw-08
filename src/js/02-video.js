15ea19a20d05a753c5b7fc90363669263a51e01b
Tetiana Kornilova <ktianaa@gmail.com>
2022-11-17 07:55:01 +0200

    adds video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+const VIDEO_KEY = 'videoplayer-current-time';
+
+var throttle = require('lodash.throttle');
+
+function onPlay(data) {
+  localStorage.setItem(VIDEO_KEY, data.seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(localStorage.getItem(VIDEO_KEY));
